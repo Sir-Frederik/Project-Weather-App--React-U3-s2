d24c7a2bd46a3d3113029665795d95a30ab6e9f8
Federico <brunetti.federico.13@gmail.com>
2025-05-23 13:10:13 +0200

    caricvati font, creato primo componente myheader

---
+++ src/components/MyHeader.jsx
@@ -0,0 +1,15 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+function MyHeader() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <img src="./src\assets\Gizmo_Umbrella.png"></img>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
+}
+export default MyHeader;
