1404826132564e3cfa56fc7b12706a717cb975aa
Federico <brunetti.federico.13@gmail.com>
2025-05-23 15:26:28 +0200

    lavorando su sfondo

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import MyHeader from "./components/MyHeader";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
+      {/*     <Container fluid>
+        {" "}
+        <img src="src\assets\clouds.png" />
+      </Container> */}
+
       <MyHeader />
     </>
   );
--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -3,20 +3,13 @@ import { Col, Container, Row } from "react-bootstrap";
 function MyHeader() {
   return (
     <Container fluid>
-      <Row>
+      <Row className="align-items-center">
         <Col xs={3}>
-          <img src="./src\assets\Gizmo_Umbrella.png"></img>
+          <img src="./src/assets/Gizmo_Umbrella.png" alt="Gizmo con ombrello" className="img-fluid" style={{ maxWidth: "100%", height: "auto" }} />
         </Col>
         <Col xs={9}>
-          <Row>
-            <Col xs={12}>
-              <p> Tiny Umbrella </p>
-            </Col>
-            <Col xs={12}>
-              {" "}
-              <p>Devo prendere l'ombrello?</p>
-            </Col>
-          </Row>
+          <h1 className="fs-1 mb-0">Tiny Umbrella</h1>
+          <h2 className="fs-4 mt-0">...Devo prendere l'ombrello?</h2>
         </Col>
       </Row>
     </Container>
