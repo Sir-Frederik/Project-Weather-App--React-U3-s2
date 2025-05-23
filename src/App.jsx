92d46dba465b799aa1c87c7efdf3e399e07fa556
Federico <brunetti.federico.13@gmail.com>
2025-05-23 16:37:11 +0200

    impostato routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MyHeader from "./components/MyHeader";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./css/App.css";
 import { Col, Container, Row } from "react-bootstrap";
 import MyForm from "./components/MyForm";
+import Hompeage from "./components/Hompeage";
+import Weather from "./components/Weather";
 // import Clouds from "./components/Clouds";
 
 function App() {
   return (
-    <>
-      <Container fluid>
-        <Row>
-          <Col>
-            <MyHeader />
-          </Col>
-          <Col xs={12}>
-            {" "}
-            <MyForm />
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {" "}
+        <Route path="/homepage" element={<Hompeage />} />
+        <Route path="/weather" element={<Weather />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Hompeage.jsx
@@ -0,0 +1,17 @@
+const Hompeage = () => {
+  return (
+    <Container fluid>
+      <Row>
+        <Col>
+          <MyHeader />
+        </Col>
+        <Col xs={12}>
+          {" "}
+          <MyForm />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Hompeage;
