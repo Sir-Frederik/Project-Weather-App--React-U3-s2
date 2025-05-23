9a5b31f8fa0a5edad790574ef0887e9f436d8986
Federico <brunetti.federico.13@gmail.com>
2025-05-23 20:16:03 +0200

    parametir passati con navigate, fetxh fatta in homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,7 @@
 import { Col, Container, Row, Form, FormControl } from "react-bootstrap";
 import { useState } from "react";
 import MyHeader from "./MyHeader";
-import { useNavigate } from "react-router-dom";
+import { Await, useNavigate } from "react-router-dom";
 
 const Homepage = () => {
   const navigate = useNavigate();
@@ -9,9 +9,30 @@ const Homepage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    navigate(`/weather/${long}/${lat}`);
+    searchLocation(searchQuery);
   };
 
+  const searchLocation = (query) => {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&appid=2f26da6fb39093c3a6b6f3b61989f718`, {})
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+          console.log(resp);
+        }
+      })
+
+      .then((location) => {
+        console.log("location" + location);
+        const lon = location[0].lon;
+        const lat = location[0].lat;
+        console.log("coordinate: " + lat + " " + lon);
+
+        navigate(`/weather/${lat}/${lon}`);
+      })
+      .catch((error) => {
+        console.error("Si è verificato un errore:", error);
+      });
+  };
   return (
     <Container fluid>
       <Row>
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,6 +1,4 @@
-import React from "react";
 import { Container } from "react-bootstrap";
-import img404 from "../assets/404.jpg";
 
 const NotFound = () => {
   return <Container fluid className="ErrorContainer"></Container>;
