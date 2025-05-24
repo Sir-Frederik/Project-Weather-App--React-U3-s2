359ada86eb8c994c002469ca3491fa7c175c1333
Federico <brunetti.federico.13@gmail.com>
2025-05-24 20:19:44 +0200

    lavorando sull'aspetto di weather

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,14 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import rainImg from "../assets/WeatherIcon/Rain.png";
 
 const Weather = () => {
   const navigate = useNavigate();
   const { lat, lon } = useParams();
-  const [locationWeather, setLocationWeather] = useState("");
+  const [weatherCondition, setWeatherCondition] = useState("");
+  const [locationName, setLocationName] = useState("");
+  const [temperature, setTemperature] = useState("");
 
   const searchWeather = (lat, lon) => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2f26da6fb39093c3a6b6f3b61989f718&units=metric&lang=it`, {})
@@ -17,8 +21,9 @@ const Weather = () => {
       .then((weather) => {
         console.log("ciao sono searchWeather e ti do i parametri: " + lat + " " + lon);
 
-        setLocationWeather(weather.weather[0].description);
-        console.log(locationWeather);
+        setWeatherCondition(weather.weather[0].description);
+        setLocationName(weather.name);
+        setTemperature(weather.main.temp);
       })
       .catch((error) => {
         console.error("Si è verificato un errore:", error);
@@ -30,7 +35,24 @@ const Weather = () => {
     searchWeather(lat, lon);
   });
 
-  return <p>{locationWeather}</p>;
+  return (
+    <>
+      <Container className="text-center mt-5  ">
+        <Row className="align-items-center g-0 ">
+          <Col xs={12}>
+            <h3> {locationName} </h3>
+          </Col>
+          <Col xs={12} md={6} className="d-flex justify-content-center">
+            <img src={rainImg} className="img-fluid w-25 rounded" />
+          </Col>
+          <Col xs={12} md={6} className="d-flex align-items-center justify-content-center">
+            <h3 className="text-center"> {temperature} </h3>
+            {/* <p> °C</p> */}
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Weather;
