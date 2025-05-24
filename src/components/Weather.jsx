153e950f556eb47e0622a79c4e5336035d77ee42
Federico <brunetti.federico.13@gmail.com>
2025-05-24 22:04:29 +0200

    lavorando ad un modo per cambiare icona in base al meteo. creato weatherimgtype. Mettere tutto ini public?

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,14 +2,17 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import rainImg from "../assets/WeatherIcon/Rain.png";
+import cloudsImg from "../assets/WeatherIcon/clouds.png";
 import umbrellaCoyote from "../assets/wile_umbrella.png";
 
 const Weather = () => {
   const navigate = useNavigate();
   const { lat, lon } = useParams();
-  const [weatherCondition, setWeatherCondition] = useState("");
+  const [weatherDescription, setWeatherDescription] = useState("");
+  const [weatherType, setWeatherType] = useState("");
   const [locationName, setLocationName] = useState("");
   const [temperature, setTemperature] = useState("");
+  let weatherImgType = "";
 
   const searchWeather = (lat, lon) => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2f26da6fb39093c3a6b6f3b61989f718&units=metric&lang=it`, {})
@@ -22,9 +25,15 @@ const Weather = () => {
       .then((weather) => {
         console.log("ciao sono searchWeather e ti do i parametri: " + lat + " " + lon);
 
-        setWeatherCondition(weather.weather[0].description);
+        setWeatherDescription(weather.weather[0].description);
+        setWeatherType(weather.weather[0].main);
         setLocationName(weather.name);
         setTemperature(weather.main.temp);
+
+        if (weatherType === "Clouds") {
+          weatherImgType = cloudsImg;
+          console.log(weatherImgType);
+        }
       })
       .catch((error) => {
         console.error("Si è verificato un errore:", error);
@@ -40,18 +49,17 @@ const Weather = () => {
     <>
       <Container className="mt-5">
         <Row className="align-items-start">
-          {/* Colonna Coyote */}
           <Col xs="auto" className="d-flex justify-content-center">
             <img src={umbrellaCoyote} alt="Coyote" className="img-fluid" style={{ width: "200px" }} />
           </Col>
-          {/* Colonna Meteo */}
+
           <Col>
             <div className="text-center">
               <h3 className="fw-bold mb-4" style={{ fontSize: "2.2rem" }}>
                 {locationName}
               </h3>
               <div className="d-flex flex-column flex-md-row align-items-center justify-content-center weather-block-lg">
-                <img src={rainImg} className="img-fluid weatherImg mb-2 mb-md-0 me-md-4" alt="Meteo Icon" />
+                <img src={weatherImgType} className="img-fluid weatherImg mb-2 mb-md-0 me-md-4" alt="Meteo Icon" />
                 <span className="d-flex flex-column align-items-center">
                   <h2 className="temperature mb-0">{temperature}</h2>
                   <small className="unit" style={{ marginTop: "-0.5em" }}>
