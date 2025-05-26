f833c6cb74c2f7b8753b2b1db1cba278c9917b7d
Federico <brunetti.federico.13@gmail.com>
2025-05-26 12:30:36 +0200

    aumentata dimensione h1

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -8,8 +8,8 @@ function MyHeader() {
           <img src="./src/assets/Gizmo_Umbrella.png" alt="Gizmo con ombrello" className="img-fluid" style={{ maxWidth: "100%", height: "auto" }} />
         </Col>
         <Col xs={9}>
-          <h1 className="fs-1 ">Tiny Umbrella</h1>
-          <h2 className="fs-4 mt-1">...Devo prendere l'ombrello?</h2>
+          <h1>Tiny Umbrella</h1>
+          <h2 className="fs-3 mt-2">...Devo prendere l'ombrello?</h2>
         </Col>
       </Row>
     </Container>
